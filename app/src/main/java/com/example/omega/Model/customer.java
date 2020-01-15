192ab46c6882ebef259e3815fb154b1255e7a9d9
Muhammed <rishdhimuhammed@gmail.com>
2020-01-15 18:10:18 +0530

    Revert "Initial commit"
    This reverts commit d5e0f642

---
+++ app/src/main/java/com/example/omega/Model/customer.java
@@ -0,0 +1,49 @@
+package com.example.omega.Model;
+
+import com.orm.SugarRecord;
+
+public class customer extends SugarRecord<customer>{
+    private String username,email,phoneNumber,password;
+
+    public customer() {
+    }
+
+    public customer(String username,String email,String phoneNumber,String password) {
+        this.username = username;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.password=password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/example/omega/Model/product.java
@@ -0,0 +1,10 @@
+package com.example.omega.Model;
+
+public class product {
+    String productName,productCategory,productDescription,image;
+    double productPrice;
+    int quantity;
+    Boolean availability;
+
+
+}
