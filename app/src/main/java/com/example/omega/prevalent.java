baee680bb24380675b878ef07d8b16383c060551
Muhammed <rishdhimuhammed@gmail.com>
2020-01-23 00:23:41 +0530

    The drawer is added to the home

---
+++ app/src/main/java/com/example/omega/prevalent.java
@@ -0,0 +1,10 @@
+package com.example.omega;
+
+import com.example.omega.Model.customer;
+
+public class prevalent {
+    public static customer currentOnlineCustomer;
+
+    public static final String UserEmail = "UserEmail";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/com/example/omega/ui/productViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.omega.ui;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class productViewHolder extends RecyclerView.ViewHolder {
+    ImageView imageView;
+
+    public productViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
